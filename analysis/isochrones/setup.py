0193e5c37cf97b62df962f5a67ee642151249db5
Timothy Morton <tmorton@ufl.edu>
2019-05-14 12:59:26 -0700

    add isochrones setup

---
+++ analysis/isochrones/setup.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from isochrones.cluster import StarCatalog
+
+df = pd.read_csv('../../data/k2_input_isochrones.csv', index_col='id')
+df['parallax'] *= 1000
+df['parallax_unc'] *= 1000
+bands = ['g', 'r', 'J', 'H', 'K', 'Bp', 'Rp']  #, 'B', 'V']
+k2cat = StarCatalog(df, props=['parallax'], bands=bands)
+k2cat.write_ini(root='results/isochrones')
